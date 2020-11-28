a027dd237713ee0e5b3dc3fdce5d9733ac8d01fa
Felix Kim <felix.l.kim@gmail.com>
2020-11-27 23:05:46 -0500

    Initial user profile layout

--- app/src/main/java/edu/utap/nutrino/ui/UserProfileFragment.kt
+++ app/src/main/java/edu/utap/nutrino/ui/UserProfileFragment.kt
@@ -2,35 +2,25 @@ package edu.utap.nutrino.ui
 
 import android.content.DialogInterface
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Button
-import android.widget.EditText
+import android.widget.GridView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
-import androidx.recyclerview.widget.LinearLayoutManager
-import edu.utap.nutrino.BuildConfig
-import edu.utap.nutrino.MainActivity
+import com.google.android.material.color.MaterialColors.getColor
+import com.google.firebase.auth.FirebaseAuth
 import edu.utap.nutrino.R
-import kotlinx.android.synthetic.main.fragment_rv.*
 import kotlinx.android.synthetic.main.user_profile_frag.*
-import java.util.*
 
 class UserProfileFragment : Fragment() {
 
-    private val restarantTypes: Array<String> by lazy {
+    private val dietTypes: Array<String> by lazy {
         resources.getStringArray(R.array.diet_type)
-    }
-    private val order: Array<String> by lazy {
-        resources.getStringArray(R.array.order)
-    }
-    private val limit: Array<String> by lazy {
-        resources.getStringArray(R.array.limit)
     }
 
     companion object {
@@ -39,34 +29,74 @@ class UserProfileFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.user_profile_frag, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val dietTypeAdapter = ArrayAdapter.createFromResource(context!!,
-                R.array.diet_type,
-                android.R.layout.simple_spinner_item)
+        var displayName = FirebaseAuth.getInstance().currentUser!!.displayName
+        if (displayName.isNullOrBlank()) {
+            userProfileTV.text = "User Profile"
+        }
+        else {
+            userProfileTV.text = displayName + "'s " + "Profile"
+        }
+
+        val dietTypeAdapter = ArrayAdapter.createFromResource(
+            context!!,
+            R.array.diet_type,
+            android.R.layout.simple_spinner_item
+        )
         dietTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         diet_type_spinner.adapter = dietTypeAdapter
-
-        val limitAdapter = ArrayAdapter.createFromResource(context!!,
-                R.array.limit,
-                android.R.layout.simple_spinner_item)
-        limitAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        limit_spinner.adapter = limitAdapter
-
-        val orderAdapter = ArrayAdapter.createFromResource(context!!,
-                R.array.order,
-                android.R.layout.simple_spinner_item)
-        orderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        choose_order_spinner.adapter = orderAdapter
 
         help_button.setOnClickListener {
             giveDietInfo()
         }
+
+        val intolerancesAdapter = ArrayAdapter.createFromResource(
+            context!!,
+            R.array.intolerance_candidates,
+            android.R.layout.simple_list_item_multiple_choice
+        )
+        intolerancesGrid.adapter = intolerancesAdapter
+        intolerancesGrid.choiceMode = GridView.CHOICE_MODE_MULTIPLE
+
+        back_button.setOnClickListener {
+            parentFragmentManager.popBackStack()
+        }
+
+        clear_button.setOnClickListener {
+            // Uncheck all the checkboxes
+            for (i in 0 until intolerancesAdapter.count) {
+                intolerancesGrid.setItemChecked(i, false)
+            }
+            // Set diet type back to default
+            diet_type_spinner.setSelection(0)
+        }
+
+        save_button.setOnClickListener {
+            val builder = AlertDialog.Builder(context!!)
+            builder.setMessage("Please confirm that you want to save your profile as it currently is.")
+                .setNegativeButton(R.string.diet_update_cancel,
+                    DialogInterface.OnClickListener { dialog, id ->
+                        // User cancelled the dialog
+                    })
+                .setPositiveButton(R.string.diet_update_confirm,
+                    DialogInterface.OnClickListener { dialog, id ->
+                        updateProfile()
+                    })
+            // Create the AlertDialog object and return it
+            builder.create().show()
+        }
+
+        presentProfile()
     }
 
     private fun giveDietInfo() {
@@ -92,7 +122,6 @@ class UserProfileFragment : Fragment() {
             }
         }
 
-        //Toast.makeText(context, dietInfoString, Toast.LENGTH_LONG).show()
         builder.setMessage(dietInfoString)
             .setNegativeButton(R.string.diet_info_cancel,
                 DialogInterface.OnClickListener { dialog, id ->
@@ -100,14 +129,12 @@ class UserProfileFragment : Fragment() {
                 })
         // Create the AlertDialog object and return it
         builder.create().show()
+    }
 
-        /*
+    private fun updateProfile() {
+        Toast.makeText(context, "Your profile has been updated!", Toast.LENGTH_SHORT).show()
+    }
 
-        builder.setMessage(dietInfoString)
-
-        // Create the AlertDialog object and return it
-        builder.create()
-
-         */
+    private fun presentProfile() {
     }
 }
